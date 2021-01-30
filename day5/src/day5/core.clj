a717291ef1d20e5e863c4eec583a0d123e57002e
hagenek <georghagen@gmail.com>
2021-01-30 15:56:18 +0100

    some progress

--- day5/src/day5/core.clj
+++ day5/src/day5/core.clj
@@ -1,5 +1,5 @@
 (ns day5.core
-  (:gen-class))
+  (:require [clojure.math.numeric-tower :as math]))
 
 ;; BFFFBBFRRR: row 70, column 7, seat ID 567.
 ;; FFFBBBFRRR: row 14, column 7, seat ID 119.
@@ -7,11 +7,19 @@
 
 (def test-input "BFFFBBFRRR\r\nFFFBBBFRRR\r\nBBFFBBFRLL\r\n")
 (def test-string "BBFFBBFRLL")
-(def real-input slurp " ../../resources/input.txt")
+(def real-input (slurp " ../../resources/input.txt"))
+
+(defn progress-rowfind [val char]
+  (if (= char \B)
+    (/ val 2)
+    val))
+
+(defn return-upper-half [num] num)
+(defn return-lower-half [num] (math/floor (/ num 2)))
 
 (defn find-row
   [seq-string]
-  (let [row-seq (take 7 seq-string)]
-    row-seq))
+  (let [row-seq (vec (take 7 seq-string))]
+    (println row-seq)
+    (reduce progress-rowfind 128 row-seq)))
 
-(find-row "BFFFBBFRRR")
