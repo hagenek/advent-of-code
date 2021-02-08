9d87cea0d8c9512c2a34405d4cb7cbb02e009693
hagenek <georghagen@gmail.com>
2021-02-08 12:26:26 +0100

    solved day1 with C#

---
+++ 2020/cSharp/Day01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var ints = File.ReadAllLines("input.txt")
+      .Select(s => Int32.Parse(s)).ToList();
+      for (int i = 0; i < ints.Count; i++)
+      {
+        var firsts = ints[i];
+        for (int j = i + 1; j < ints.Count; j++)
+        {
+          for (int k = j + 1; k < ints.Count; k++)
+          {
+            if (ints[i] + ints[j] + ints[k] == 2020)
+            {
+              Console.WriteLine("Found it!" + (ints[i] * ints[j] * ints[k]));
+            }
+          }
+        }
+      }
+    }
+  }
+}
