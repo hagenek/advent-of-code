0541e488befed8abc592ce0dc911f0673eac59cc
hagenek <georghagen@gmail.com>
2021-02-08 12:38:13 +0100

    refactored into two static methods for part1 and part2

--- 2020/cSharp/Day01/Program.cs
+++ 2020/cSharp/Day01/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
+
 
 namespace Day01
 {
@@ -9,21 +11,41 @@ namespace Day01
     static void Main(string[] args)
     {
       var ints = File.ReadAllLines("input.txt")
-      .Select(s => Int32.Parse(s)).ToList();
+        .Select(s => Int32.Parse(s)).ToList();
+      SolvePart2(ints);
+    }
+
+    static int SolvePart2(List<int> ints)
+    {
       for (int i = 0; i < ints.Count; i++)
       {
-        var firsts = ints[i];
-        for (int j = i + 1; j < ints.Count; j++)
+        for (int j = i + 1; j < ints.Count(); j++)
         {
-          for (int k = j + 1; k < ints.Count; k++)
+          for (int k = j + 1; k < ints.Count(); k++)
           {
             if (ints[i] + ints[j] + ints[k] == 2020)
             {
-              Console.WriteLine("Found it!" + (ints[i] * ints[j] * ints[k]));
+              return ints[i] * ints[j] * ints[k];
             }
           }
         }
       }
+      return 0;
+    }
+
+    static int SolvePart1(List<int> ints)
+    {
+      for (int i = 0; i < ints.Count; i++)
+      {
+        for (int j = i + 1; j < ints.Count(); j++)
+        {
+          if (ints[j] + ints[i] == 2020)
+          {
+            return ints[i] * ints[j];
+          }
+        }
+      }
+      return 0;
     }
   }
 }
